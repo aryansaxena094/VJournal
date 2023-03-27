4285efbf0de81ec586d4e5377f31db42afc61c4d
Aryan <aryan.saxena094@gmail.com>
2023-03-27 02:32:53 -0400

    solo posts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { forEach, lowerCase } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -29,6 +30,18 @@ app.get("/contact", function(req, res){
 
 app.get("/compose", function(req, res){
   res.render("compose");
+});
+
+app.get("/posts/:title",function(req, res){
+  let requestedtitle = lowerCase(req.params.title);
+
+  posts.forEach(function(post){
+    const storedTitle = lowerCase(post.title);
+    if(storedTitle === requestedtitle){
+      res.render("post",{post_title : post.title, post_content : post.content});
+    }
+  });
+  
 });
 
 app.post("/compose", function(req, res){
